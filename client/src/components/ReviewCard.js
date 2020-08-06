0f46741f4a7a56d7d1bf4dc0a2a876e936ef43d7
kundanchandel <kundanchandel08@gmail.com>
2020-08-06 17:59:28 +0530

    updated review card

--- client/src/components/ReviewCard.js
+++ client/src/components/ReviewCard.js
@@ -15,33 +15,48 @@ function ReviewCard({
 }) {
   return (
     <div className="row reviewCard">
-      <div className="col-2 avatar">
-        <img alt="avatar" src={image} />
-      </div>
-      <div className="col-10 content">
-        <div className="row">
-          <div className="col-6">
-            <h5>{name}</h5>
+      <div className="row">
+        <div className="col-2 avatar">
+          <img alt="avatar" src={image} />
+        </div>
+        <div className="col-10">
+          <div className="row">
+             <h5>{name}</h5>
           </div>
-          <div className="col-6">
-            <StarRatings
+          <div className="row">
+          <StarRatings
               rating={rating}
               starRatedColor="orange"
               starDimension="20px"
               starSpacing="5px"
             />
           </div>
+              
         </div>
-        <h6>{title}</h6>
-        <p>{description}</p>
-        <div className="row">
-          <div className="col-12">
+      </div>
+      <div style={{width:"100%", height:"2px",backgroundColor:"grey", marginTop:"2px"}}></div>
+      <div className="row content pt-3">
+          <div className="row pl-5">
+                <h6>{title}</h6>
+          </div>
+          <div className="row pl-5">
+                <p>{description}</p> 
+          </div>
+          
+        <div className="row iconRow">
+          <div className="col-6 col-sm-4">
+                <button className="icons useful"><FaRegThumbsUp/> USEFUL</button>
+          </div>
+          <div className="col-6 col-sm-4">
+                <button className="icons notUseFul"><FaRegThumbsDown/> NOT USEFUL</button>
+          </div>
+          <div className="col-12 col-sm-4">
             {auth.user._id === comment.author ? (
               <button className="delete" onClick={() => onDelete(comment._id)}><FaRegTrashAlt/></button>
             ) : (
               ""
             )}
-          </div>
+          </div>  
         </div>
       </div>
     </div>
